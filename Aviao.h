3cdd0be303814a155a22c7ac06c84666d82afcc2
Megabytes <Megabytes@192.168.0.6>
2016-01-24 21:36:15 -0200

    Aviao

---
+++ Aviao.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Aviao
+{
+	private: 
+	
+		turbina int;
+		passageiros int;
+		autonomia float;
+		
+	public:
+		
+		Aviao(); 
+		
+		void ativar_turbina();
+		void freiar();
+		void decolar();
+		void aterrissar();
+    
+};
