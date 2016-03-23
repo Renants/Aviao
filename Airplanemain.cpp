98a6b8ef2b7156222f980d04fe7a29c283c8edc8
Renants <renannojosa@gmail.com>
2016-03-23 17:33:54 -0300

    Update Airplanemain.cpp

--- Airplanemain.cpp
+++ Airplanemain.cpp
@@ -20,7 +20,7 @@ main(){
 	Data Obj_blackbox(01,01,2001);
     string nome;
     linha(25);
-	cout << "\n*Bem vindo ao Helios One Aircraft*\n";
+	cout << "\n*Bem vindo ao Helios One*\n";
 	linha(25);
 	
     vector<Airplane*> Avioes;
@@ -29,7 +29,7 @@ main(){
     Avioes.push_back(new Jatinho());
     Avioes.push_back(new JatoComercial());
     
-    Drone *Raptor = dynamic_cast < Drone * > (Airplane);
+    Drone *Raptor = dynamic_cast < Drone * > (Airplane); // dynamic_cast
     Jatinho *Concorde = dynamic_cast < Jatinho * > (Airplane);
     JatoComercial *Helios_One = dynamic_cast < JatoComercial * > (Jatinho);
     
