fd7e6b8318eb1e90271128298f8c3c76ea90ad0b
Renants <renannojosa@gmail.com>
2016-03-28 16:07:55 -0300

    Update Jatinho.h

--- Jatinho.h
+++ Jatinho.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 using namespace std;
-class Jatinho : public Airplane
+class Jatinho : public Airplane // classe abstrata
 {
        // friend ostream& operator<<(ostream &, const Jatinho &);
 public:
@@ -14,6 +14,16 @@ public:
     Jatinho(int , int, int );
     ~Jatinho();
     string retModelo();
+    virtual void    visorMensagem() = 0;
+    virtual void    visor() = 0;
+    virtual void    alterarEscalas() = 0;
+    virtual void    alterarGps() = 0;
+    virtual void    alterarPilotoauto() = 0;
+    virtual void    alterarAlcance () = 0;
+    virtual void    decolar() = 0;
+    virtual void    alterarVoo() = 0;
+    virtual void	alterarVelocidade() = 0;
+    virtual void    alterarTransponder() = 0;
 
 protected:
     string modelo;
