80ece5f0be5155a91ccb8760f4531f19554fb33f
Renants <renannojosa@gmail.com>
2016-03-28 16:14:44 -0300

    Update Raptor.h

--- Raptor.h
+++ Raptor.h
@@ -3,7 +3,7 @@
 #include "Drone.h"
 #include <string>
 #include <iostream>
-class Raptor: public Drone
+class Raptor: public Drone // classe concreta
 { friend ostream& operator<<(ostream &, const Raptor &);
 public:
     Raptor( int 1000,int 7);
@@ -12,17 +12,17 @@ public:
     bool operator==(const Raptor &) const;
 	Drone(int , int );
     Raptor( const Raptor & );
-    virtual void defDispararp() = 0;
-    virtual void defDispararm() = 0;
-	virtual void visor() = 0;
-    virtual void visorMensagem() =0;
-	virtual void adicionarAlvos() = 0 ;
-    virtual void alterarGps() = 0;
-    virtual void alterarPilotoauto() = 0;
-    virtual void alterarAlcance () = 0;
-    virtual void alterarVelocidade() = 0;
-    virtual void decolar() = 0;
-    virtual void alterarVoo() = 0;
+    virtual void defDispararp();
+    virtual void defDispararm();
+	virtual void visor();
+    virtual void visorMensagem();
+	virtual void adicionarAlvos();
+    virtual void alterarGps();
+    virtual void alterarPilotoauto();
+    virtual void alterarAlcance ();
+    virtual void alterarVelocidade();
+    virtual void decolar();
+    virtual void alterarVoo();
     	    void alterarStealth();
 	
 private:
